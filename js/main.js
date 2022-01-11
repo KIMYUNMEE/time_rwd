9de965e837e418e5c8de14faca5d1db21ab86e68
KIMYUNMEE <jayoo8665@naver.com>
2022-01-11 18:15:55 +0900

    시간보정 함수 주석버전

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const menus = document.querySelector('nav span');
 const numbers = document.querySelectorAll('.screen span');
 
 setInterval(()=>{  
+  //setTime함수로 시간, 분,초 값 배열로 반환
   const times = setTime();
+  //반환된 숫자값들을 반복을 돌며 값을 보정하여 선택자의 순번에 맞게 적용
   times.forEach((time,index) =>getTime(time, index));
 
 
@@ -25,6 +27,7 @@ setInterval(()=>{
   }  
 },1000)
 
+//시간값을 구해서 [시간,분,초]로 반환하는 함수
 function setTime(){
   let now = new Date();
   let hr = now.getHours();
@@ -33,17 +36,9 @@ function setTime(){
   return [hr, min, sec];
 }
 
+//변환할 숫자값과 순서값을 인수로 받아
+//숫자를 보정하고 해당 인수의 부모요소에 값 적용하는 함수
 function getTime(num,index){
   if(num<10) num = '0'+num;
   numbers[index].innerText = num;
 }
-
- /*
-  if(hr < 10) hr = '0'+hr;
-  if(min < 10) min = '0'+min;
-  if(sec < 10) sec = '0'+sec;
-  
-  time[0].innerText = hr;
-  time[1].innerText = min;
-  time[2].innerText = sec;
-  */
