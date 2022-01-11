5eaf9fd1f8ca861e7624a55efefd12ac398ad36b
KIMYUNMEE <jayoo8665@naver.com>
2022-01-11 18:13:46 +0900

    시간에 따라 최상위 부모요소 클래스명 변경

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-﻿const time = document.querySelectorAll('.screen span');
+﻿const main = document.querySelector('main');
+const menus = document.querySelector('nav span');
+const time = document.querySelectorAll('.screen span');
 
 setInterval(()=>{
   let now = new Date();
@@ -6,27 +8,27 @@ setInterval(()=>{
   let min = now.getMinutes();
   let sec = now.getSeconds();
 
+  if(hr>=5 && hr<11){
+    main.className = '';
+    main.classList.add('morning')
+  }
+  if(hr>=11 && hr<16){
+    main.className='';
+    main.classList.add('afternoon');
+  }
+  if(hr>=16 && hr<19){
+    main.className='';
+    main.classList.add('evening');
+  }
+  if(hr>=19 || hr<5){
+    main.className='';
+    main.classList.add('night');
+  }
+
   if(hr < 10) hr = '0'+hr;
   if(min < 10) min = '0'+min;
   if(sec < 10) sec = '0'+sec;
-
   time[0].innerText = hr;
   time[1].innerText = min;
   time[2].innerText = sec;
 },1000)
-
-//미션1 - 각 시간값이 한자리일때는 앞쪽에 0을 추가해서 출력
-
-
-
-
-
-
-
-
-
-
-
-
-
-
