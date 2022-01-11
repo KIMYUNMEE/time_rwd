1366fd985e7be8e9b6468e13891cd4eddf153a18
KIMYUNMEE <jayoo8665@naver.com>
2022-01-11 18:12:20 +0900

    시간 출력

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-﻿
+﻿const time = document.querySelectorAll('.screen span');
+
+setInterval(()=>{
+  let now = new Date();
+  let hr = now.getHours();
+  let min = now.getMinutes();
+  let sec = now.getSeconds();
+
+  if(hr < 10) hr = '0'+hr;
+  if(min < 10) min = '0'+min;
+  if(sec < 10) sec = '0'+sec;
+
+  time[0].innerText = hr;
+  time[1].innerText = min;
+  time[2].innerText = sec;
+},1000)
+
+//미션1 - 각 시간값이 한자리일때는 앞쪽에 0을 추가해서 출력
 
 
 
