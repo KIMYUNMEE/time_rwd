3b865776126aed73b17e0ab1bb287e3141dba2b5
KIMYUNMEE <jayoo8665@naver.com>
2022-01-11 18:18:52 +0900

    낮 테마일때 글자색 어둡게 처리

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 ﻿const main = document.querySelector('main');
 const menus = document.querySelectorAll('nav span');
 const numbers = document.querySelectorAll('.screen span');
+const [am, pm] = document.querySelectorAll('.screen em');
 
 setInterval(()=>{ 
   const times = setTime();
   let hr = times[0];
   times.forEach((time,index) =>getTime(time, index));
-
   //조건식과 그에 매칭되는 클래스명을 객체형태로 묶어서
   //배열로 저장
   const data = [
@@ -15,19 +15,24 @@ setInterval(()=>{
     {condition: hr>=16 && hr<19, name: 'evening'},
     {condition: hr>=19 || hr<5, name: 'night'},
   ]
-
   //해당 배열을 반복돌면서 main, menus를 활성화
   data.forEach((item, index)=>{
     if(item.condition){
       main.className='';
       main.classList.add(item.name);
-
       for(let menu of menus) menu.classList.remove('on');
       menus[index].classList.add('on');
     }
-  }) 
+  })  
 
+  //현재 메인의 클래스명이 afternoon일때만 추가로 dark_txt클래스 추가
+  if(main.classList.contains('afternoon')){
+    main.classList.add('dark_text');
+  }else{
+    main.classList.remove('dark_text');
+  }
 },1000)
+
 
 
 function setTime(){
@@ -35,7 +40,20 @@ function setTime(){
   let hr = now.getHours();
   let min = now.getMinutes();
   let sec = now.getSeconds();
-  return [hr, min, sec];
+  let hr2 = null;
+
+
+  if(hr > 12){  
+    hr2 = hr-12;  
+    pm.classList.add('on');
+    am.classList.remove('on')
+  }   
+  else{
+    hr2 = hr;
+    am.classList.add('on');
+    pm.classList.remove('on');
+  }
+  return [hr2, min, sec];
 }
 
 function getTime(num,index){
