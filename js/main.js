b5c01cb375a325b18841d302c6d4782ac6a74662
KIMYUNMEE <jayoo8665@naver.com>
2022-01-11 18:16:35 +0900

    조건문 반복처리

--- js/main.js
+++ js/main.js
@@ -1,33 +1,35 @@
 ﻿const main = document.querySelector('main');
-const menus = document.querySelector('nav span');
+const menus = document.querySelectorAll('nav span');
 const numbers = document.querySelectorAll('.screen span');
 
-setInterval(()=>{  
-  //setTime함수로 시간, 분,초 값 배열로 반환
+setInterval(()=>{ 
   const times = setTime();
-  //반환된 숫자값들을 반복을 돌며 값을 보정하여 선택자의 순번에 맞게 적용
+  let hr = times[0];
   times.forEach((time,index) =>getTime(time, index));
 
+  //조건식과 그에 매칭되는 클래스명을 객체형태로 묶어서
+  //배열로 저장
+  const data = [
+    {condition: hr>=5 && hr<11, name: 'morning'},
+    {condition: hr>=11 && hr<16, name: 'afternoon'},
+    {condition: hr>=16 && hr<19, name: 'evening'},
+    {condition: hr>=19 || hr<5, name: 'night'},
+  ]
 
-  if(hr>=5 && hr<11){
-    main.className = '';
-    main.classList.add('morning')
-  }
-  if(hr>=11 && hr<16){
-    main.className='';
-    main.classList.add('afternoon');
-  }
-  if(hr>=16 && hr<19){
-    main.className='';
-    main.classList.add('evening');
-  }
-  if(hr>=19 || hr<5){
-    main.className='';
-    main.classList.add('night');
-  }  
+  //해당 배열을 반복돌면서 main, menus를 활성화
+  data.forEach((item, index)=>{
+    if(item.condition){
+      main.className='';
+      main.classList.add(item.name);
+
+      for(let menu of menus) menu.classList.remove('on');
+      menus[index].classList.add('on');
+    }
+  }) 
+
 },1000)
 
-//시간값을 구해서 [시간,분,초]로 반환하는 함수
+
 function setTime(){
   let now = new Date();
   let hr = now.getHours();
@@ -36,8 +38,6 @@ function setTime(){
   return [hr, min, sec];
 }
 
-//변환할 숫자값과 순서값을 인수로 받아
-//숫자를 보정하고 해당 인수의 부모요소에 값 적용하는 함수
 function getTime(num,index){
   if(num<10) num = '0'+num;
   numbers[index].innerText = num;
